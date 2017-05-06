4196a7fb650f65f51bf059aa490b24fdeabfdd8b
wg4568 <wg4568@gmail.com>
2017-05-05 23:38:35 -0700

    added stellar.log

--- stellar/__init__.py
+++ stellar/__init__.py
@@ -1,3 +1,5 @@
+import inspect
+import time
 import base
 import hitboxes
 import keys
@@ -8,3 +10,10 @@ import objects
 import sound
 
 __authors__ = ["LeapBeforeYouLook", "Ramaraunt"]
+
+def log(*msgs):
+	curframe = inspect.currentframe()
+	calframe = inspect.getouterframes(curframe, 2)
+	calname = calframe[1][3]
+	string = "[STELLAR] (%s - %s) %s" % (calname, time.time(), ", ".join(msgs))
+	print string
