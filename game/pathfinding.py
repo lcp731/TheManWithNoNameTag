4b2fe89b5af9d6b1d83cf814f5a9360b48291a76
wg4568 <wg4568@gmail.com>
2017-05-17 14:50:59 -0700

    pathfinding

--- game/pathfinding.py
+++ game/pathfinding.py
@@ -1,2 +1,18 @@
 import room_game
-grid = room_game.Room().grid
+import tools
+
+class Tile:
+	def __init__(self, x, y, solid=False):
+		self.x = x
+		self.y = y
+		self.solid = solid
+
+grid = {}
+for x, y in tools.itergrid(50, 50):
+	grid[x, y] = Tile(x, y)
+
+def get_next_step(grid, start, end):
+	# do some magic
+	return (0, 0)
+
+print get_next_step(grid, (0, 0), (5, 5))
